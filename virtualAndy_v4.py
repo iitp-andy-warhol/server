32a72460ca9247bdb9c1ad6bf49f7e68a23ecfa9
jiminsun <jiminxsun@gmail.com>
2020-02-03 14:26:29 -0500

    fix va

--- virtualAndy_v4.py
+++ virtualAndy_v4.py
@@ -303,8 +303,9 @@ get_drive = True
 ccw = True
 mmode_flag = False
 stop = True
-good_to_go_loading = False
-good_to_go_unloading = False
+global good_to_go_loading, good_to_go_unloading
+# good_to_go_loading = False
+# good_to_go_unloading = False
 
 current_path_id = None
 current_path = None
