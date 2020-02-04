91eef480cd4ef91b503fb54a630a2cb673f9b604
reonaledo <reonaledo@gmail.com>
2020-02-04 15:57:34 -0500

    '기능추가.리드미참고'

--- con_db.py
+++ con_db.py
@@ -5,21 +5,46 @@ import pandas as pd
 
 action = sys.argv[1]
 
-def db(action):
+try:
+    orderid = sys.argv[2]
+except:
+    orderid = None
+
+def db(action, orderid):
     cnx = mysql.connector.connect(host=host, user=user, password=passwd, database=schema)
     cursor = cnx.cursor()
 
-    if action == 'd':
-        query = 'delete from orders where id != 000;'
+    if action in ['d','ㅇ']:
+        if orderid is None:
+            query = 'delete from orders where id != 000;'
+            orderid = 'All'
+        else:
+            query = f'delete from orders where id ={orderid};'
         cursor.execute(query)
         cnx.commit()
-        print('All rows have been deleted.')
+        print(f'id = {orderid} row(s) have been deleted.')
 
-    elif action == 's':
-        query = f"SELECT {', '.join(colname)} " + "FROM orders"
+    elif action in ['s', 'ㄴ']:
+        if orderid is None:
+            query = f"SELECT {', '.join(colname)} " + "FROM orders;"
+        else:
+            query = f"SELECT {', '.join(colname)} " + f"FROM orders WHERE id={orderid};"
+
         cursor.execute(query)
         pending = cursor.fetchall()
         pending = pd.DataFrame(pending, columns=colname)
+        print(pending)
+
+    elif action in ['sa', 'ㄴㅁ']:
+
+        if orderid is None:
+            query = f"SELECT * FROM orders;"
+        else:
+            query = f"SELECT * FROM orders WHERE id={orderid};"
+
+        cursor.execute(query)
+        pending = cursor.fetchall()
+        pending = pd.DataFrame(pending, columns=fulcolname)
         print(pending)
 
 if __name__ == '__main__':
@@ -30,5 +55,7 @@ if __name__ == '__main__':
     query = ''
     colname = ['id', 'address', 'red', 'green', 'blue', 'required_red', 'required_green',
                                 'required_blue', 'orderdate']
+    fulcolname = ['id', 'exp_id', 'customer', 'address', 'orderdate', 'r', 'g', 'b', 'f_r', 'f_g', 'f_b', 'r_r', 'r_g', 'r_b',
+                  'pending', 'filldate', 'tot_item']
 
-    db(action)
+    db(action, orderid)
