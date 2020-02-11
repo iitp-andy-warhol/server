dc1f284c58100db86e980593c56fbb48d2b2779e
reonaledo <reonaledo@gmail.com>
2020-02-11 12:18:29 -0500

    'ordersetidlist'

--- robotstatus.py
+++ robotstatus.py
@@ -1,17 +1,16 @@
 import time
 
 
-def makeRobotStatus(direction, current_address, action, current_basket, operating_orderset, operating_order,next_orderset, ping):
+def makeRobotStatus(direction, current_address, action, current_basket, operating_orderset, operating_order,orderset_id_list, ping):
     l = [6, 0, 1, 2, 3, 4, 5, 6, 0]
     dic ={
         'direction' : direction,
         'current_address' : current_address,
-        #'next_address' : l[current_address+1+direction],
         'action' : action,
         'current_basket' : current_basket,
         'operating_orderset' : operating_orderset,
         'operating_order' : operating_order,
-        'next_orderset': next_orderset,
+        'orderset_id_list': orderset_id_list,
         'log_time' : time.strftime('%c', time.localtime(time.time())),
         'ping' : ping
     }
